93b094450c6661295749d139d10cccc66e0be844
Jason Doanh <>
2021-10-14 17:30:40 +0700

    VND Textfield

---
+++ VNDTextFile/Sources/VNDTextFile/VNDTextFile.swift
@@ -0,0 +1,3 @@
+struct VNDTextFile {
+    var text = "Hello, World!"
+}
---
+++ VNDTextFile/Tests/VNDTextFileTests/VNDTextFileTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import VNDTextFile
+
+    final class VNDTextFileTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(VNDTextFile().text, "Hello, World!")
+        }
+    }
